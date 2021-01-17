3bde32b8cab01ca47deb75b963916d1c7b1a7de9
Eivind Yu Nilsen <eivindyunilsen@hotmail.com>
2021-01-17 02:14:28 +0100

    Remove HDRP and FMOD and Add VRTK and basic VR scene

---
+++ Assets/EiT/Scripts/PickUpItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    public List<GameObject> items = new List<GameObject>();
+    public void PickUp(GameObject item) {
+        items.Add(item);
+        Debug.Log("Picked up " + item.name);
+        Destroy(item);
+    }
+}
